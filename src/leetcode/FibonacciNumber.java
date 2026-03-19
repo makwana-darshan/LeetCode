f92cf5052b8e0ba86b226bec0ca0cf68c1ca0687
MAKWANADARSHAN <makwanadarshan6113@gmail.com>
2026-03-19 09:12:13 +0530

    509

---
+++ src/leetcode/FibonacciNumber.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class FibonacciNumber {
+    public int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int prev1 = 1;
+        int prev2 = 0;
+        for (int i = 2; i <= n; i++) {
+            int current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+}
