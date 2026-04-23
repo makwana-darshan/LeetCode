e5d8fabd2daef04da6d3e77da787bea0587d64a6
makwana-darshan <makwanadarshan901>
2026-04-23 08:05:58 +0530

    1004

---
+++ src/leetcode/MaxConsecutiveOnes.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class MaxConsecutiveOnes {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0;
+        int zeros = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zeros++;
+            }
+            while (zeros > k) {
+                if (nums[left] == 0) {
+                    zeros--;
+                }
+                left++;
+            }
+            maxLen = Math.max(maxLen, i - left + 1);
+        }
+        return maxLen;
+    }
+}
