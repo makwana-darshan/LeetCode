f5f011dd03dc0fcff716d33a3e25e1331f6c390d
makwana-darshan <makwanadarshan901>
2026-04-25 10:00:02 +0530

    643

---
+++ src/leetcode/MaximumAverageSubarray.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class MaximumAverageSubarray {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        double maxAvg = (double) sum / k;
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i];
+            sum -= nums[i - k];
+            maxAvg = Math.max(maxAvg, (double) sum / k);
+        }
+        return maxAvg;
+    }
+}
