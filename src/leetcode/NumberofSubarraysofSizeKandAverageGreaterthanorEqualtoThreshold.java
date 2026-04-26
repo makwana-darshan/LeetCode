939afd14cdf7ab1ca78158787bbf1a6d1090d3ec
makwana-darshan <makwanadarshan901>
2026-04-26 08:33:36 +0530

    1343

---
+++ src/leetcode/NumberofSubarraysofSizeKandAverageGreaterthanorEqualtoThreshold.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class NumberofSubarraysofSizeKandAverageGreaterthanorEqualtoThreshold {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        if ((double) sum / k >= threshold) {
+            count++;
+        }
+        for (int i = k; i < arr.length; i++) {
+            sum += arr[i];
+            sum -= arr[i - k];
+            if ((double) sum / k >= threshold) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
