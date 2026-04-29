7cb9c8820de45feb7ae33f15e493db44a4980b2b
makwana-darshan <makwanadarshan901>
2026-04-29 08:06:14 +0530

    2485

---
+++ src/leetcode/FindThePivotInteger.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class FindThePivotInteger {
+    public int pivotInteger(int n) {
+        int sum = n * (n + 1) / 2;
+        int sqrt = (int) Math.sqrt(sum);
+        if (sqrt * sqrt == sum) {
+            return sqrt;
+        } else {
+            return -1;
+        }
+    }
+}
