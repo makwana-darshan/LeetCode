4aa54bad82fc5486acc79c0123f4f8ee31cc102f
makwana-darshan <makwanadarshan901>
2026-05-16 08:18:50 +0530

    1668

--- src/leetcode/MaximumRepeatingSubstring.java
+++ src/leetcode/MaximumRepeatingSubstring.java
@@ -1,4 +1,26 @@
 package leetcode;
 
 public class MaximumRepeatingSubstring {
+    public int maxRepeating(String sequence, String word) {
+        int n = sequence.length();
+        int m = word.length();
+        int[] dp = new int[n + 1];
+        int maxK = 0;
+
+        for (int i = m; i <= n; i++) {
+            if (sequence.substring(i - m, i).equals(word)) {
+                dp[i] = dp[i - m] + 1;
+                maxK = Math.max(maxK, dp[i]);
+            }
+        }
+        return maxK;
+    }
+
+    public int maxRepeating1(String sequence, String word) {
+        int count = 0;
+        while (sequence.contains(word.repeat(count + 1))) {
+            count++;
+        }
+        return count;
+    }
 }
