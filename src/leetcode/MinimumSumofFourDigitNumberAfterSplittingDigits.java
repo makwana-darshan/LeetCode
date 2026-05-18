0665b9ea5e56cf84dd16ec8d1e3f949ce05d30d4
makwana-darshan <makwanadarshan901>
2026-05-18 09:43:51 +0530

    2160

---
+++ src/leetcode/MinimumSumofFourDigitNumberAfterSplittingDigits.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class MinimumSumofFourDigitNumberAfterSplittingDigits {
+    public int minimumSum(int num) {
+        int[] nums = new int[4];
+        int i = 0;
+        while (num > 0) {
+            nums[i] = num % 10;
+            num /= 10;
+            i++;
+        }
+        Arrays.sort(nums);
+        int new1 = nums[0] * 10 + nums[2];
+        int new2 = nums[1] * 10 + nums[3];
+
+        return new1 + new2;
+    }
+}
