d462300bcc3b0fcc2d79a5fd080b2da9fa5da4a7
makwana-darshan <makwanadarshan901>
2026-05-20 11:46:10 +0530

    2011

---
+++ src/leetcode/FinalValueVariableAfterPerformingOperations.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class FinalValueVariableAfterPerformingOperations {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (String op : operations) {
+            if (op.contains("+")) {
+                x++;
+            } else {
+                x--;
+            }
+        }
+        return x;
+    }
+}
