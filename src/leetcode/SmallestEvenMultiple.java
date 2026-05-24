965f2fd153e387cf20856fc19a598d52a41385cb
makwana-darshan <makwanadarshan901>
2026-05-24 08:36:45 +0530

    2413

---
+++ src/leetcode/SmallestEvenMultiple.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class SmallestEvenMultiple {
+    public int smallestEvenMultiple(int n) {
+        if (n % 2 == 0) {
+            return n;
+        } else {
+            return n * 2;
+        }
+    }
+}
