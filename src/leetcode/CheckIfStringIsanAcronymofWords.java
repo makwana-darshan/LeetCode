71b3fe6bf53816f21dc4fc90beaccbbc2f7ab13b
makwana-darshan <makwanadarshan901>
2026-06-03 17:52:13 +0530

    2828

---
+++ src/leetcode/CheckIfStringIsanAcronymofWords.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.List;
+
+public class CheckIfStringIsanAcronymofWords {
+    public boolean isAcronym(List<String> words, String s) {
+        if (words.size() != s.length()) {
+            return false;
+        }
+        for (int i = 0; i < words.size(); i++) {
+            if (words.get(i).charAt(0) != s.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
