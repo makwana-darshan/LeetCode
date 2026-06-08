954ab9294d7f4d3d3309fccb4fad77701e0788f7
makwana-darshan <makwanadarshan901>
2026-06-08 13:11:14 +0530

    2469

---
+++ src/leetcode/ConvertTheTemperature.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+public class ConvertTheTemperature {
+    public double[] convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32.00;
+
+        return new double[]{kelvin, fahrenheit};
+    }
+}
