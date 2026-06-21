5040b8a40ee5b7895149ca32d2eb0899152b1eb0
makwana-darshan <makwanadarshan901>
2026-06-21 17:40:34 +0530

    2760

---
+++ src/leetcode/LongestEvenOddSubarrayWithThreshold.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class LongestEvenOddSubarrayWithThreshold {
+    public int longestAlternatingSubarray(int[] nums, int threshold) {
+        for (int i = 0; i < nums.length; i++) {
+            
+        }
+    }
+}
