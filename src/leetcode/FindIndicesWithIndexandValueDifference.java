d34121e75858821a5588391b7f03e0d1c7917165
makwana-darshan <makwanadarshan901>
2026-06-28 19:16:04 +0530

    2903

---
+++ src/leetcode/FindIndicesWithIndexandValueDifference.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class FindIndicesWithIndexandValueDifference {
+    public int[] findIndices(int[] nums, int indexDifference, int valueDifference) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + indexDifference; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) >= valueDifference) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
