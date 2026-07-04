bc494d1804e79e43ba56e9a18c58b77ff3539e35
makwana-darshan <makwanadarshan901>
2026-07-04 14:34:41 +0530

    2980

---
+++ src/leetcode/CheckifBitwiseORHasTrailingZeros.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class CheckifBitwiseORHasTrailingZeros {
+    public boolean hasTrailingZeros(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if ((num & 1) == 0) {
+                count++;
+            }
+        }
+        return count >= 2;
+    }
+}
