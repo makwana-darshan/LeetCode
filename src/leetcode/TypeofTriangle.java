2ee09a314aca8fe71ecf61c1de2142ad27c29fbe
makwana-darshan <makwanadarshan901>
2026-07-07 12:20:05 +0530

    3024

---
+++ src/leetcode/TypeofTriangle.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class TypeofTriangle {
+    public String triangleType(int[] nums) {
+        if (nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[1] + nums[2] > nums[0]) {
+            if (nums[0] == nums[1] && nums[0] == nums[2]) {
+                return "equilateral";
+            }
+            if (nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]) {
+                return "isosceles";
+            }
+            return "scalene";
+        }
+        return "none";
+    }
+}
