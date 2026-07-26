4a9ed1964a46c574dca92173c05ac3ca55c6ca06
makwana-darshan <makwanadarshan901>
2026-07-26 08:59:57 +0530

    3190

---
+++ src/leetcode/FindMinimumOperationstoMakeAllElementsDivisiblebyThree.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class FindMinimumOperationstoMakeAllElementsDivisiblebyThree {
+    public int minimumOperations(int[] nums) {
+        int count=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (!(nums[i]%3==0)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
