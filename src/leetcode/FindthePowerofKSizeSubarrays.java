aba96d0de14068dc5733d924b83c9f7f759714db
makwana-darshan <makwanadarshan901>
2026-08-04 08:36:53 +0530

    3254

---
+++ src/leetcode/FindthePowerofKSizeSubarrays.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class FindthePowerofKSizeSubarrays {
+    public int[] resultsArray(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        for (int i = 0; i <= n - k; i++) {
+            boolean isValid = true;
+            for (int j = i + 1; j < n + k; j++) {
+                if (nums[j] != nums[j - 1] + 1) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) {
+                result[i] = nums[i + k - 1];
+            } else {
+                result[i] = -1;
+            }
+        }
+        return result;
+    }
+}
