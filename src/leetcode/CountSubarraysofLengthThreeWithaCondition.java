096829c2e25a7e39a06af1b77b5da012dc3e9b7d
makwana-darshan <makwanadarshan901>
2026-08-24 08:42:03 +0530

    3392

---
+++ src/leetcode/CountSubarraysofLengthThreeWithaCondition.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class CountSubarraysofLengthThreeWithaCondition {
+    public int countSubarrays(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        for (int i = 1; i < n - 1; i++) {
+            if ((nums[i - 1] + nums[i + 1]) * 2 == nums[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
