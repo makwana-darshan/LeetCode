54e31e71bd661ba02cf1a9d3710ff00e1aaf92f6
makwana-darshan <makwanadarshan6113@gmail.com>
2026-08-31 19:22:25 +0530

    3427

---
+++ src/leetcode/SumofVariableLengthSubarrays.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class SumofVariableLengthSubarrays {
+    public int subarraySum(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        int totalSum = 0;
+        for (int i = 0; i < n; i++) {
+            int start = Math.max(0, i - nums[i]);
+            totalSum += prefix[i + 1] - prefix[start];
+        }
+        return totalSum;
+    }
+}
